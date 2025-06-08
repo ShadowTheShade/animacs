de2633cd5e47998f2034e2a9da4009d4425a6315
Anthony Gagliano <antogagliano3@gmail.com>
2025-06-07 21:29:01 -0400

    feature: capability to fetch list of candidates for a chosen anime and display the list of results in a buffer.

---
+++ animacs.el
@@ -0,0 +1,93 @@
+;;; animacs.el --- a GNU Emacs package for browsing and watching anime -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025 ShadowTheShade
+
+;; Author: ShadowTheShade <antogagliano3@gmail.com>
+;; URL: https://github.com/ShadowTheShade/animacs
+;; Version: 0.1
+
+(require 'json)
+(require 'url-util)
+(require 'plz)
+
+(defgroup animacs nil
+  "Select and watch anime within Emacs."
+  :group 'multimedia)
+
+(defconst allanime-agent
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/121.0")
+(defconst allanime-refr "https://allmanga.to")
+(defconst allanime-base "allanime.day")
+(defconst allanime-api (concat "https://api." allanime-base "/api"))
+
+(defun generate-search-variables (query mode)
+  (json-encode
+   `(("search" . (("allowAdult" . :json-false)
+                  ("allowUnknown" . :json-false)
+                  ("query" . ,query)))
+     ("limit" . 40)
+     ("page" . 1)
+     ("translationType" . ,mode)
+     ("countryOrigin" . "ALL"))))
+
+(defun generate-api-url (search-gql variables)
+  (format "%s?query=%s&variables=%s"
+          allanime-api
+          (url-hexify-string search-gql)
+          (url-hexify-string variables)))
+
+(defun search-anime (query mode)
+  "Search AllAnime using QUERY string and MODE (e.g., \"sub\" or \"dub\")."
+  (let* ((search-gql "query( $search: SearchInput $limit: Int $page: Int $translationType: VaildTranslationTypeEnumType $countryOrigin: VaildCountryOriginEnumType ) { shows( search: $search limit: $limit page: $page translationType: $translationType countryOrigin: $countryOrigin ) { edges { _id name availableEpisodes __typename } }}")
+         (variables (generate-search-variables query mode))
+         (url (generate-api-url search-gql variables))
+         (response (plz 'get url
+                     :headers `(("User-Agent" . ,allanime-agent)
+                                ("Referer" . ,allanime-refr))
+                     :decode 'json))
+	 (hashed-data (json-parse-string response))
+	 (hashed-shows (gethash "edges" (gethash "shows" (gethash "data" hashed-data)))))
+    hashed-shows))
+
+(defun create-anime-list (hash)
+  "Convert a list of anime show hash tables into formatted strings."
+  (mapcar (lambda (show)
+            (format "Name: %s | Sub: %d | Dub: %d"
+                    (gethash "name" show)
+                    (gethash "sub" (gethash "availableEpisodes" show))
+                    (gethash "dub" (gethash "availableEpisodes" show))))  
+	  hash))
+
+(defun display-search-anime-results (query mode)
+  "Search for QUERY in given MODE and display formatted results in a buffer."
+  (interactive
+   (list (read-string "Search query: ")
+         (completing-read "Mode: " '("sub" "dub") nil t)))
+  (let* ((results (search-anime query mode))
+         (lines (create-anime-list results)))
+    (with-current-buffer (get-buffer-create "*Animacs Results*")
+      (read-only-mode -1)
+      (erase-buffer)
+      (insert (string-join lines "\n"))
+      (goto-char (point-min))
+      (read-only-mode 1)
+      (display-buffer (current-buffer)))))
+
+(provide 'animacs)
+
+;;; animacs.el ends here
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
